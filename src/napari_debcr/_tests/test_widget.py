2ca326ee26fafe903e5c5577bb37061eb1c938e6
Artsemi Yushkevich <artsemi.yushkevich@gmail.com>
2025-02-21 17:32:28 +0100

    clean tests

--- src/napari_debcr/_tests/test_widget.py
+++ src/napari_debcr/_tests/test_widget.py
@@ -1,37 +1,9 @@
 import numpy as np
 
 from napari_debcr._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
+    InferenceQWidget,
+    ImageThreshold
 )
-
-
-def test_threshold_autogenerate_widget():
-    # because our "widget" is a pure function, we can call it and
-    # test it independently of napari
-    im_data = np.random.random((100, 100))
-    thresholded = threshold_autogenerate_widget(im_data, 0.5)
-    assert thresholded.shape == im_data.shape
-    # etc.
-
-
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# you don't need to import it, as long as napari is installed
-# in your testing environment
-def test_threshold_magic_widget(make_napari_viewer):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
-
-    # our widget will be a MagicFactory or FunctionGui instance
-    my_widget = threshold_magic_widget()
-
-    # if we "call" this object, it'll execute our function
-    thresholded = my_widget(viewer.layers[0], 0.5)
-    assert thresholded.shape == layer.data.shape
-    # etc.
-
 
 def test_image_threshold_widget(make_napari_viewer):
     viewer = make_napari_viewer()
@@ -50,13 +22,13 @@ def test_image_threshold_widget(make_napari_viewer):
 
 
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
+def test_inference_q_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
     viewer.add_image(np.random.random((100, 100)))
 
     # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    my_widget = InferenceQWidget(viewer)
 
     # call our widget method
     my_widget._on_click()
