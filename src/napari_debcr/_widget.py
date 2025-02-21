58dce04a0ffc706bd2e1a6da00491104c636d50e
Artsemi Yushkevich <artsemi.yushkevich@gmail.com>
2025-02-21 17:30:45 +0100

    upd inference widget: add UI to load model weights add UI to show long logs

--- src/napari_debcr/_widget.py
+++ src/napari_debcr/_widget.py
@@ -1,15 +1,21 @@
-from typing import TYPE_CHECKING
+import os
+import glob
 
 from magicgui.widgets import CheckBox, Container, create_widget
-from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QPushButton, QComboBox, QLabel, QFileDialog, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QVBoxLayout, QPushButton, QComboBox, QTextEdit, QLabel, QFileDialog, QWidget
+from qtpy.QtCore import QTimer
+
 import napari
-from skimage.util import img_as_float
 
-import debcr
-
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import napari
 
+from napari.utils.notifications import show_info
+from skimage.util import img_as_float
+
+import debcr
+    
 class InferenceQWidget(QWidget):
     
     def __init__(self, viewer: "napari.viewer.Viewer"):
@@ -26,46 +32,136 @@ class InferenceQWidget(QWidget):
         
     def _init_layout(self):
 
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-        self.run_btn = btn
-        
-        input_data_line = QHBoxLayout()
-        input_data_line.addWidget(QLabel("Select input data:"))
+        # Button to run prediction
+        run_widget = QPushButton("Click me!")
+        run_widget.clicked.connect(self._on_run_click)
+        self.run_btn = run_widget
+
+        ## Layout for input data
+        data_layout = QVBoxLayout()
+        data_layout.addWidget(QLabel("Select input data:"))
+
+        # Drop-down to select layer as input data
         self.layer_select = QComboBox()
         self._update_layer_select()
         self.layer_select.activated.connect(self._update_layer_select)
-        input_data_line.addWidget(self.layer_select)
-
-        weight_file_line = QHBoxLayout()
-        weight_file_line.addWidget(QLabel("Load weights: "))
-        self.weight_file_button = QPushButton("Open")
-        self.weight_file_button.clicked.connect(self._on_weight_file_click)
-        weight_file_line.addWidget(self.weight_file_button)
+        data_layout.addWidget(self.layer_select)
+        ## END Layout for input data
         
+        ## Layout for model weights
+        weights_layout = QVBoxLayout()
+        
+        ### Sublayout for model weights directory
+        weights_dir_layout = QHBoxLayout()
+
+        self.choose_dir_btn = QPushButton("Choose directory")
+        self.choose_dir_btn.clicked.connect(self._on_choose_dir_click)
+        weights_dir_layout.addWidget(self.choose_dir_btn)
+        
+        self.show_dir_btn = QPushButton("Show path")
+        self.show_dir_btn.clicked.connect(self._on_show_dir_click)
+        weights_dir_layout.addWidget(self.show_dir_btn)
+        ### END Sublayout for model weights directory
+        
+        ### Sublayout for model weights file
+        weights_file_layout = QHBoxLayout()
+        
+        # Drop-down to select weights file
+        self.weights_select = QComboBox()
+        weights_file_layout.addWidget(self.weights_select)
+
+        # Button to load weights file
+        self.load_button = QPushButton("Load weights") 
+        self.load_button.clicked.connect(self._on_weights_load_click)
+        weights_file_layout.addWidget(self.load_button)
+        ### END Sublayout for model weights file
+
+        weights_layout.addWidget(QLabel("Provide weights directory:"))
+        weights_layout.addLayout(weights_dir_layout)
+        weights_layout.addWidget(QLabel("Select weights file:"))
+        weights_layout.addLayout(weights_file_layout)
+        ## END Layout for model weights
+
+        # Log box for messages for user
+        log_widget = QTextEdit()
+        log_widget.setReadOnly(True)
+        self.log_box = log_widget
+        
+        # General plugin layout
         layout = QVBoxLayout()
         self.setLayout(layout)
-        layout.addLayout(input_data_line)
-        layout.addLayout(weight_file_line)
-        layout.addWidget(btn)
+        layout.addLayout(data_layout)
+        layout.addLayout(weights_layout)
+        layout.addWidget(run_widget)
+        layout.addWidget(log_widget)
+                
+        # Store a weights dir path
+        self.current_weights_dir = None
     
-    def _on_click(self):
+    def _on_run_click(self):
         print("napari has", len(self.viewer.layers), "layers")
         print("selected layer is", len(self.layer_select.currentText()))
         print("loaded model info is ", self.debcr.summary())
 
-    def _on_weight_file_click(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getOpenFileName(
-            self, "Load weights (ckpt-*.index file)", "",
-            "All Files (*);;TensorFlow Files (*.index)",
-            options=options)
-        if file_name:
-            print(file_name)
-            debcr.load_weights()
-            #self.ufish.load_weights(file_name) # load weights via DeBCR API here!
-            
+    def _on_choose_dir_click(self):
+        dir_path = QFileDialog.getExistingDirectory(self, "Select Weights Directory")
+        
+        if dir_path:
+            self.current_weights_dir = os.path.abspath(dir_path)
+            #show_info(f'Selected path: {self.current_weights_dir}')
+            #QTimer.singleShot(500, lambda: show_info(f'Selected path: {self.current_weights_dir}'))
+            self._log_message(f'Selected weights directory path:\n{self.current_weights_dir}')
+            self._update_weights_dropdown() # update dropdown with found weight files
+     
+    def _on_show_dir_click(self):
+        
+        if self.current_weights_dir:
+            #show_info(f'Selected weights dir path: {self.current_weights_dir}')
+            #QTimer.singleShot(500, lambda: show_info(f'Selected weights dir path: {self.current_weights_dir}'))
+            self._log_message(f'Selected weights directory path:\n{self.current_weights_dir}')
+        else:
+            #show_info('No weights directory selected.')
+            QTimer.singleShot(500, lambda: show_info('No weights directory selected.'))
+    
+    def _update_weights_dropdown(self):
+        self.weights_select.clear()
+        
+        if not self.current_weights_dir:
+            return
+
+        # Find all .index files (checkpoint files)
+        weight_filepaths = sorted(glob.glob(f'{self.current_weights_dir}/*.index'))
+        
+        if weight_filepaths:
+            weight_filenames = [os.path.basename(filepath) for filepath in weight_filepaths]
+            weight_names = [filename.replace(".index", "") for filename in weight_filenames]
+            self.weights_select.addItems(weight_names)
+        else:
+            self.weights_select.addItem("No checkpoint files found.")
+
+    def _on_weights_load_click(self):
+        
+        if not self.current_weights_dir:
+            print("No weights directory selected.")
+            return
+        
+        selected_file = self.weights_select.currentText()
+
+        if selected_file == "No checkpoint files found" or not selected_file:
+            print("No valid file selected.")
+            return
+
+        checkpoint_file_prefix = selected_file.replace(".index", "")
+        checkpoint_prefix = str(f'{self.current_weights_dir}/{checkpoint_file_prefix}')
+        print(f"Loading weights from: {checkpoint_prefix}")
+
+        self.debcr = debcr.model.load(self.current_weights_dir, checkpoint_file_prefix)
+        
+        print("Weights loaded successfully!")
+
+    def _log_message(self, message):
+        self.log_box.append(f'\n{message})
+    
     def _update_layer_select(self):
         current_layer = self.layer_select.currentText()
         self.layer_select.clear()
